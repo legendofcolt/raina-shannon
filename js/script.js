182b7fbc311a7461af872ea9fa5e7bf34232fbb9
Colt <colton@fariswheel.com>
2016-08-15 23:44:45 -0500

    Move script to src script.js instead of dist script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
+// Widowtamer
+// About page header
+wt.fix({
+  elements: '.About h1',
+  chars: 15,
+  method: 'nbsp',
+  event: 'resize'
+});
 
+// About paragraphs
+wt.fix({
+  elements: '.About p',
+  chars: 15,
+  method: 'padding-right',
+  event: 'resize'
+});
