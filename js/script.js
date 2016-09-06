775b8016b2cfc12a3677669959908f88df8ee31f
legendofcolt <coltborg@gmail.com>
2016-09-05 19:06:03 -0500

    Update header
    Look through header code and improve. Add app singleton to control nav
    logic. Will add any further js logic within singleton.

--- js/script.js
+++ js/script.js
@@ -6,3 +6,28 @@ wt.fix({
   method: 'nbsp',
   event: 'resize'
 });
+
+(function() {
+  var app = {
+    // Nav
+    'nav': {
+      'navToggleEl': document.querySelector('.js-menu-toggle'),
+      'navMenuEl': document.querySelector('.js-menu'),
+      'navInit': function () {
+        app.nav.navToggleEl.addEventListener('click', app.nav.navToggle)
+      },
+      'navToggle': function () {
+          app.nav.navToggleEl.classList.toggle('is-active');
+          app.nav.navMenuEl.classList.toggle('is-active');
+      }
+    },
+    // Init
+    'init': function() {
+      app.nav.navInit();
+    }
+  };
+  window.app = app;
+})();
+
+// Move to inside IIFE at production time !!!
+app.init();
